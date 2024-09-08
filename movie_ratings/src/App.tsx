5721f36d48f6a5d0bceb4cc22c67b9d7c7cb22b8
ORNavneetRanjan <navneet.ranjan274057@gmail.com>
2024-09-08 16:50:51 +0530

    initial commit

---
+++ movie_ratings/src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import ShowDetailPage from "./Pages/ShowDetails.Page";
+import ShowListPage from "./Pages/ShowsList.Page";
+
+function App() {
+  return (
+    <div className="max-w-5xl mx-auto">
+      <Routes>
+        <Route path="/" element={<ShowListPage />} />
+        <Route path="show/:show_id" element={<ShowDetailPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
